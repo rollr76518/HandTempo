856caaf16d3d210e49ed884ad17bf9026b1ff8c1
Hanyu <rollr76518@gmail.com>
2017-09-25 23:01:18 +0800

    - 調整音符的高度為寬度的1/2 - 修改 倫敦鐵橋的音譜

--- HandTempo/ViewControllers/SongPlayerViewController.swift
+++ HandTempo/ViewControllers/SongPlayerViewController.swift
@@ -115,6 +115,6 @@ extension SongPlayerViewController: UICollectionViewDataSource, UICollectionView
     {
         let width = collectionView.bounds.size.width/4
         
-        return CGSize.init(width: width, height: width)
+        return CGSize.init(width: width, height: width/2)
     }
 }
